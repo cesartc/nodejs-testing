01f19474d9b150360049a8095d143db80ba51b38
Cesar Alfredo Tarrillo Celada <cesar@Cesars-MacBook-Pro.local>
2018-10-09 00:15:11 -0500

    feat(create): Test create new user

--- users.js
+++ users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const User = require('./models/user');
+const mailer = require('./mailer');
 
 exports.get = function (id, callback) {
   if (!id) {
@@ -12,4 +13,30 @@ exports.get = function (id, callback) {
     }
     return callback(null, result);
   });
+};
+
+exports.delete = (id) => {
+  if (!id) {
+    return Promise.reject(new Error('Invalid ID'));
+    // return Promise.reject(new Error('Invalid ID'));
+  }
+
+  return User.remove({_id: id});
+}
+
+exports.create = (data) => {
+  if (!data || !data.email || !data.name) {
+    return Promise.reject(new Error('Invalid arguments'));
+  }
+
+  const user = new User(data);
+
+  return user.save().then((result) => {
+    return mailer.sendWelcomeEmail(data.email, data.name)
+    .then(() => {
+      return {message: 'User created', userId: result.id};
+    });
+  }).catch((err) => {
+    return Promise.reject(err);
+  })
 }
--- users.test.js
+++ users.test.js
@@ -16,6 +16,7 @@ const sandbox = sinon.sandbox.create();
 
 describe('users', () => {
   let findStub;
+  let deleteStub;
   let sampleArgs;
   let sampleUser;
 
@@ -26,6 +27,7 @@ describe('users', () => {
       email: 'foo@bar.com'
     }
     findStub = sandbox.stub(mongoose.Model, 'findById').resolves(sampleUser);
+    deleteStub = sandbox.stub(mongoose.Model, 'remove').resolves('fake_remove_result');
   });
 
   afterEach(() => {
@@ -52,5 +54,39 @@ describe('users', () => {
       });
     });
 
+    it('Should catch error if there is one', () => {
+      sandbox.restore();
+      let stub = sandbox.stub(mongoose.Model, 'findById').yields(new Error('fake'));
+
+      users.get(123, (err, result) => {
+        expect(result).to.not.exist;
+        expect(err).to.exist;
+        expect(err).to.be.instanceOf(Error);
+      });
+    });
+  });
+
+  context('delete user', () => {
+    it('Should check for an ID useing return', (done) => {
+      users.delete().then((result) => {
+        throw new Error('unexpected success');
+      }).catch((err) => {
+        expect(err).to.exist;
+        expect(err).to.be.instanceOf(Error);
+        expect(err.message).to.equal('Invalid ID');
+        done();
+      });
+    });
+
+    it('Should check for errors using eventually', () => {
+      return expect(users.delete()).to.eventually.be.rejectedWith('Invalid ID');
+    });
+  });
+
+  context('create user', () => {
+    it('Should reject invalid args', async () => {
+      expect(users.create()).to.eventually.be.rejectedWith('Invalid arguments');
+      expect(users.create({email: 'user@new.com'})).to.eventually.be.rejectedWith('Invalid arguments');
+    });
   });
 })
